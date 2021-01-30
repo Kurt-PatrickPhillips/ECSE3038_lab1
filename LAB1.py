f9fce74cb7e8b269908e662b19372645ee384590
Kurt-PatrickPhillips <Kurtpatrick.phillips@gmail.com>
2021-01-29 21:35:51 -0500

    Q2 second commit

--- LAB1.py
+++ LAB1.py
@@ -5,3 +5,17 @@
 def hello():
     print("ECSE3038 - Engineering IoT Systems") #this line displays the sentence
 
+#Q2 Password Validation
+#more than 7 characters
+#atleast 2 numbers and alphanumeric
+def ValidatePassword(Pwrd):
+    Inc =0;
+
+    if len(Pwrd) > 7 and Pwrd.isalnum():
+        for val in Pwrd:
+            if element.isdigit():
+                Inc +=1
+        if Inc >=2:
+            return True
+        else:
+            return False
